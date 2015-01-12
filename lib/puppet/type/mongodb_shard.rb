enovance/puppetlabs-mongodb

Implement Sharding Support
This patch aims to support MongoDB sharding feature.

* Enable Sharding in MongoDB configuration file if enabled (disabled by
  default).
* Add a Type and a Provider to manage a MongoDB Shard resource.
* Add tests/sharding.pp to show an example of usage.
* Update the documentation about MongoDB Sharding usage.
* Unit tests for the new Type and the new Provider.

lib/puppet/type/mongodb_shard.rb
@@ -1,0 +1,35 @@
+#
+# Author: Emilien Macchi <emilien.macchi@enovance.com>
+#
+
+Puppet::Type.newtype(:mongodb_shard) do
+  @doc = "Manage a MongoDB Shard"
+
+  ensurable do
+    defaultto :present
+
+    newvalue(:present) do
+      provider.create
+    end
+  end
+
+  newparam(:name) do
+    desc "The name of the replicaSet to shard"
+  end
+
+  newproperty(:members, :array_matching => :all) do
+    desc "The shard members"
+
+    def insync?(is)
+      is.sort == should.sort
+    end
+  end
+
+  autorequire(:package) do
+    'mongodb_client'
+  end
+
+  autorequire(:service) do
+    'mongodb'
+  end
+end

cegeka/puppet-mongodb

lib/puppet/type/mongodb_shard.rb
@@ -1,0 +1,39 @@
+#
+# Author: Emilien Macchi <emilien.macchi@enovance.com>
+#
+
+Puppet::Type.newtype(:mongodb_shard) do
+  @doc = "Manage a MongoDB Shard"
+
+  ensurable do
+    defaultto :present
+
+    newvalue(:present) do
+      provider.create
+    end
+  end
+
+  newparam(:name) do
+    desc "The name of the shard"
+  end
+
+  newproperty(:member) do
+    desc "The shard member"
+  end
+
+  newproperty(:keys, :array_matching => :all) do
+    desc "The sharding keys"
+
+    def insync?(is)
+      is.sort == should.sort
+    end
+  end
+
+  autorequire(:package) do
+    'mongodb_client'
+  end
+
+  autorequire(:service) do
+    'mongodb'
+  end
+end